71cf4349d91685a176cde79248bce5552ca2c29f
chahoud <sebastien.chahoud@epfl.ch>
2024-01-01 13:31:44 +0100

    gurbi added to setup file+

--- src/setup_path.m
+++ src/setup_path.m
@@ -4,5 +4,8 @@ import casadi.*
 
 tbxmanager restorepath
 x = sdpvar
-optimize (abs ( x ) <= 1 , (x -3)*(x-3))
+optimize (abs(x) <= 1 , (x-3)*(x-3))
 value(x)
+
+gruobi_setup_path = "/home/sebastien/gurobi1100/linux64/matlab/gurobi_setup.m";
+run(gruobi_setup_path)
