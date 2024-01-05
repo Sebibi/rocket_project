56709953e98c5e3b68f2c23555f660f904ae1275
chahoud <sebastien.chahoud@epfl.ch>
2024-01-05 18:17:06 +0100

    start deliverable 6

--- Deliverable_6_1/Deliverable_6_1.m
+++ Deliverable_6_1/Deliverable_6_1.m
@@ -1,7 +1,21 @@
-addpath(fullfile('..', 'src'));
+Ts = 1/20;
+rocket = Rocket(Ts);
 
-%close all
-%clear all
-%clc
+H = 2; % Horizon length in seconds
+nmpc = NmpcControl(rocket, H);
 
-%% TODO: This file should produce all the plots for the deliverable
+% MPC reference with default maximum roll = 15 deg
+ref = @(t_, x_) ref_TVC(t_);
+
+% MPC reference with specified maximum roll = 50 deg
+% roll_max = deg2rad(50);
+% ref = @(t_, x_) ref_TVC(t_, roll_max);
+
+% Evaluate once and plot optimal open−loop trajectory,
+% pad last input to get consistent size with time and state
+[u, T_opt, X_opt, U_opt] = nmpc.get_u(x, ref);
+U_opt(:,end+1) = nan;
+ph = rocket.plotvis(T_opt, X_opt, U_opt, ref);
+
+Tf = 30;
+[T, X, U, Ref] = rocket.simulate(x0, Tf, @nmpc.get_u, ref);
