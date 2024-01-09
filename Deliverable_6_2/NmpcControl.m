463e0b6dbc055affedc25784d8bb79ad01a19937
chahoud <sebastien.chahoud@epfl.ch>
2024-01-09 02:05:47 +0100

    added nmpc 6.2

---
+++ Deliverable_6_2/NmpcControl.m
@@ -0,0 +1,273 @@
+classdef NmpcControl < handle
+    
+    properties
+        solver
+        nx, nu, N
+        nlp_x0
+        nlp_lbx, nlp_ubx
+        nlp_lbg, nlp_ubg
+        nlp_p
+        
+        T_opt
+        sol
+        idx
+        
+        % Delay compensation
+        rocket
+        expected_delay
+        mem_u
+        
+        % Warmstart
+        nlp_lam_x0
+        nlp_lam_g0
+    end
+    
+    methods
+        function obj = NmpcControl(rocket, tf, expected_delay)
+           
+            if nargin < 3, expected_delay = 0; end
+
+            import casadi.*
+            
+            N_segs = ceil(tf/rocket.Ts); % MPC horizon
+            nx = 12; % Number of states
+            nu = 4;  % Number of inputs
+            
+            % Decision variables (symbolic)
+            N = N_segs + 1; % Index of last point
+            X_sym = SX.sym('X_sym', nx, N); % state trajectory
+            U_sym = SX.sym('U_sym', nu, N-1); % control trajectory)
+            
+            % Parameters (symbolic)
+            x0_sym  = SX.sym('x0_sym', nx, 1);  % initial state
+            ref_sym = SX.sym('ref_sym', 4, 1);  % target position
+            
+            % Default state and input constraints
+            ubx = inf(nx, 1);
+            lbx = -inf(nx, 1);
+            ubu = inf(nu, 1);
+            lbu = -inf(nu, 1);
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE 
+
+            % Compute the terminal cost
+            [xs, us] = rocket.trim();
+ 
+            sys = rocket.linearize(xs, us);
+            Q = diag([1, 1, 1, 1, 1, 2000, 1, 1, 1, 1000, 1000, 2000]);
+            R = diag([1, 1, 0.5, 1]);
+            sys = c2d(sys,rocket.Ts);
+            [~,Qf,~] = dlqr(sys.A, sys.B, Q, R);
+
+            % Define the discretized dynamics using RK4
+            F = @(x, u) RK4(x, u, rocket);
+            
+            % Cost
+            cost = 0;
+            
+            % Equality constraints (Casadi SX), each entry == 0            
+            % Inequality constraints (Casadi SX), each entry <= 0
+
+            % For box constraints on state and input, overwrite entries of
+            % lbx, ubx, lbu, ubu defined above
+
+            % Input constraints
+            lbu(1) = -deg2rad(15); ubu(1) = deg2rad(15);
+            lbu(2) = -deg2rad(15); ubu(2) = deg2rad(15);
+            lbu(3) = 50; ubu(3) = 80;
+            lbu(4) = -20; ubu(4) = 20;
+
+            % State constraints
+            lbx(5) = -deg2rad(75); ubx(5) = deg2rad(75);
+
+            % Reference state
+            ref_sym_all = [0; 0; 0; 0; 0; ref_sym(4); 0; 0; 0; ref_sym(1); ref_sym(2); ref_sym(3)];
+            
+            eq_constr = [X_sym(:, 1) - x0_sym; X_sym(:,2) - F(X_sym(:,1), U_sym(:,1))];
+            ineq_constr = [lbu - U_sym(:,1); U_sym(:,1) - ubu];
+            cost = cost + U_sym(:, 1)' * R * U_sym(:, 1);
+            for i = 2:N-1
+                eq_constr = [eq_constr ; X_sym(:, i+1) - F(X_sym(:,i), U_sym(:,i))];
+                ineq_constr = [ineq_constr ; lbx(5) - X_sym(5,i); X_sym(5,i) - ubx(5); lbu - U_sym(:,i); U_sym(:,i) - ubu];
+                cost = cost + (X_sym(:, i) - ref_sym_all)' * Q * (X_sym(:, i)-ref_sym_all) + U_sym(:, i)' * R * U_sym(:, i); 
+            end     
+            ineq_constr = [ineq_constr ; lbx(5) - X_sym(5,N); X_sym(5,N) - ubx(5)];
+            cost = cost + (X_sym(:, N) - ref_sym_all)'*Qf* (X_sym(:, N) - ref_sym_all); 
+            
+
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            % ---- Assemble NLP ------
+            nlp_x = [X_sym(:); U_sym(:)];
+            nlp_p = [x0_sym; ref_sym];
+            nlp_f = cost;
+            nlp_g = [eq_constr; ineq_constr];
+            
+            nlp = struct('x', nlp_x, 'p', nlp_p, 'f', nlp_f, 'g', nlp_g);
+            
+            % ---- Setup solver ------
+            opts = struct('ipopt', struct('print_level', 0), 'print_time', false);
+            obj.solver = nlpsol('solver', 'ipopt', nlp, opts);
+            
+            % ---- Assemble NLP bounds ----
+            obj.nlp_x0  = zeros(size(nlp_x));
+            
+            obj.nlp_ubx = [repmat(ubx, N, 1); repmat(ubu, (N-1), 1)];
+            obj.nlp_lbx = [repmat(lbx, N, 1); repmat(lbu, (N-1), 1)];
+            
+            obj.nlp_ubg = [zeros(size(eq_constr)); zeros(size(ineq_constr))];
+            obj.nlp_lbg = [zeros(size(eq_constr)); -inf(size(ineq_constr))];
+            
+            obj.nlp_p = [zeros(size(x0_sym)); zeros(size(ref_sym))];
+            
+            obj.nlp_lam_x0 = [];
+            obj.nlp_lam_g0 = [];
+            
+            obj.nx = nx;
+            obj.nu = nu;
+            obj.N = N;
+            obj.T_opt = linspace(0, N * rocket.Ts, N);
+            
+            obj.idx.X = [1, obj.N * obj.nx];
+            obj.idx.U = obj.idx.X(2) + [1, (obj.N-1) * obj.nu];
+            obj.idx.u0 = obj.idx.U(1) + [0, obj.nu-1];
+            
+            % Members for delay compensation
+            obj.rocket = rocket;
+            obj.expected_delay = expected_delay;
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+
+            u_init = zeros(4, 1); % Replace this by a better initialization
+            
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            obj.mem_u = repmat(u_init, 1, expected_delay);
+        end
+        
+        function [u, T_opt, X_opt, U_opt] = get_u(obj, x0, ref)
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            delay = obj.expected_delay;
+            mem_u = obj.mem_u;
+            
+            % Delay compensation: Predict x0 delay timesteps later.
+            % Simulate x_ for 'delay' timesteps
+            
+            % Define the discretized dynamics using RK4
+            F = @(x, u) RK4(x, u, obj.rocket);
+
+            x_ = x0;
+            for i = 1:delay
+                x_ = F(x_, mem_u(:, i));
+            end
+
+            x0 = x_;
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            % Compute solution from x0
+            obj.solve(x0, ref);
+            
+            % Evaluate u0
+            nlp_x = obj.sol.x;
+            id = obj.idx.u0;
+            u = full( nlp_x(id(1):id(2)) );      
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            % Delay compensation: Save current u
+            if obj.expected_delay > 0
+               obj.mem_u = [mem_u(:, 1:end-1), u];
+            end
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            if nargout > 1, T_opt = obj.get_T_opt(); end
+            if nargout > 2, X_opt = obj.get_X_opt(); end
+            if nargout > 3, U_opt = obj.get_U_opt(); end
+            return
+            
+            % Additional evaluation
+            % Complete trajectory
+            % % X_opt = full(reshape(nlp_x(idx_X(1):idx_X(2)), obj.nx, obj.N));
+            % % U_opt = full(reshape(nlp_x(idx_U(1):idx_U(2)), obj.nu, obj.N - 1));
+            % %
+            % % cost_opt = full(sol.f);
+            % % constr_opt = full(sol.g);
+            % %
+            % % stats = obj.solver.stats;
+        end
+        
+        function solve(obj, x0, ref)
+            
+            % ---- Set the initial state and reference ----
+            obj.nlp_p = [x0; ref];     % Initial condition
+            obj.nlp_x0(1:obj.nx) = x0; % Initial guess consistent
+            
+            % ---- Solve the optimization problem ----
+            args = {'x0', obj.nlp_x0, ...
+                'lbg', obj.nlp_lbg, ...
+                'ubg', obj.nlp_ubg, ...
+                'lbx', obj.nlp_lbx, ...
+                'ubx', obj.nlp_ubx, ...
+                'p', obj.nlp_p, ...
+                %                 'lam_x0', obj.nlp_lam_x0, ...
+                %                 'lam_g0', obj.nlp_lam_g0
+                };
+            
+            obj.sol = obj.solver(args{:});
+            if obj.solver.stats.success ~= true
+                solve_status_str = obj.solver.stats.return_status;
+                fprintf([' [' class(obj) ': ' solve_status_str '] ']);
+                obj.sol.x(obj.idx.u0) = nan;
+            end
+            
+            % Use the current solution to speed up the next optimization
+            obj.nlp_x0 = obj.sol.x;
+            obj.nlp_lam_x0 = obj.sol.lam_x;
+            obj.nlp_lam_g0 = obj.sol.lam_g;
+        end
+        function T_opt = get_T_opt(obj)
+            T_opt = obj.T_opt;
+        end
+        function X_opt = get_X_opt(obj)
+            nlp_x = obj.sol.x;
+            id = obj.idx.X;
+            X_opt = full(reshape(nlp_x(id(1):id(2)), obj.nx, obj.N));
+        end
+        function U_opt = get_U_opt(obj)
+            nlp_x = obj.sol.x;
+            id = obj.idx.U;
+            U_opt = full(reshape(nlp_x(id(1):id(2)), obj.nu, obj.N - 1));
+        end
+    end
+end
+
+
+function [x_next] = RK4(x, u,rocket)
+%
+% Inputs : 
+%    X, U current state and input
+%    h    sample period
+%    f    continuous time dynamics f(x,u)
+% Returns
+%    State h seconds in the future
+%
+
+% Runge-Kutta 4 integration
+% write your function here
+
+% x_next = ...
+   
+    h = rocket.Ts;
+    [k1, ~] = rocket.f(x, u);
+    [k2, ~] = rocket.f(x + (h/2)*k1, u);
+    [k3, ~] = rocket.f(x + (h/2)*k2 ,u);
+    [k4, ~] = rocket.f(x + h*k3, u);
+    x_next = x + h * (k1/6 + k2/3 + k3/3 + k4/6);
+end
+
